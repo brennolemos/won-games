c44b39a797efc7fd40213e6d19745561832f5392
Breno Lemos <brenomiros@gmail.com>
2023-02-05 16:02:15 -0300

    add: logic of opening/closing the menu

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -1,32 +1,42 @@
+import { useState } from 'react'
 import { Menu2 as MenuIcon } from 'styled-icons/remix-fill'
 import {
   ShoppingCart as ShoppingCartIcon,
-  Search as SearchIcon
+  Search as SearchIcon,
+  Close as CloseIcon
 } from 'styled-icons/material-outlined'
 
 import Logo from 'components/Logo'
 import * as S from './styles'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
-
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
 
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open Shopping Cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
--- client/src/components/Menu/test.tsx
+++ client/src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
@@ -10,5 +10,26 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // select the MenuFull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // check if the menu is hidden
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // click on open menu button and check if the it has opened
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // click on close menu button and check if the it has closed
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
