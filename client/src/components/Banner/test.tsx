f0bd31090e19efee10e52c8f6bac7d52b805a689
Breno Lemos <brenomiros@gmail.com>
2023-02-21 15:19:01 -0300

    test: adding ribbon tests to banner

--- client/src/components/Banner/test.tsx
+++ client/src/components/Banner/test.tsx
@@ -27,4 +27,21 @@ describe('<Banner />', () => {
       screen.getByRole('img', { name: bannerMock.title })
     ).toBeInTheDocument()
   })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...bannerMock}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/my ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({ height: '2.6rem', fontSize: '1.2rem' })
+  })
 })
