016283dc4c2ddb33dca8cc07e761d5734e62c0fd
Breno Lemos <brenomiros@gmail.com>
2023-02-28 22:48:08 -0300

    slider test

--- client/src/components/Slider/test.tsx
+++ client/src/components/Slider/test.tsx
@@ -1,13 +1,18 @@
+import 'match-media-mock'
 import { render, screen } from '@testing-library/react'
 
 import Slider from '.'
 
 describe('<Slider />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Slider />)
+    render(
+      <Slider settings={{ slidesToShow: 2 }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>
+    )
 
-    expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByText(/item 1/i)).toBeInTheDocument()
+    expect(screen.getByText(/item 2/i)).toBeInTheDocument()
   })
 })
