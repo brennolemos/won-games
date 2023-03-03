07404b4a52c9434b8b22f6f0ec6a8e21e4cf8385
Breno Lemos <brenomiros@gmail.com>
2023-03-03 08:52:36 -0300

    feat: add game card slider component

---
+++ client/src/components/GameCardSlider/index.tsx
@@ -0,0 +1,32 @@
+import GameCard, { GameCardProps } from 'components/GameCard'
+import Slider, { SliderSettings } from 'components/Slider'
+import {
+  ArrowBackIos as ArrowLeft,
+  ArrowForwardIos as ArrowRight
+} from 'styled-icons/material-outlined'
+import * as S from './styles'
+
+export type GameCardSliderProps = {
+  items: GameCardProps[]
+  color?: 'white' | 'black'
+}
+
+const settings: SliderSettings = {
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  nextArrow: <ArrowRight aria-label="next games" />,
+  prevArrow: <ArrowLeft aria-label="previous games" />
+}
+
+const GameCardSlider = ({ items, color = 'black' }: GameCardSliderProps) => (
+  <S.Wrapper color={color}>
+    <Slider settings={settings}>
+      {items.map((item, index) => (
+        <GameCard key={index} {...item} />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
+
+export default GameCardSlider
